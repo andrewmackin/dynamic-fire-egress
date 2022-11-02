fde88db4f94c242ac91bd4f266217aa8df586edb
Andrew Mackin <andrwmack@gmail.com>
2022-11-02 17:35:26 -0400

    remove pytest - for now

---
+++ rpi/app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print("Data received from Webhook is: ", request.json)
+        return "Webhook received!"
+
+app.run(host='localhost', port=8000)
---
+++ rpi/send_request.py
@@ -0,0 +1,4 @@
+import requests
+
+r = requests.post('http://0.0.0.0:8000/webhook', json={"key": "value"})
+dfsdf = sdfsdf
